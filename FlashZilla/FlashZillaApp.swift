926ca44648395835da2262035f0f155a8b7d141c
Weerawut Chaiyasomboon <weerawut@live.com>
2024-11-18 08:38:53 +0700

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Weerawut Chaiyasomboon on 18/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
